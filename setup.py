0eadc3bc70a48e0eee82d15921c7b66cac816406
MBAHu <nb705033848@gmail.com>
2024-03-26 19:43:57 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Bittu Hussain",
+    author_email="nb705033848@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
